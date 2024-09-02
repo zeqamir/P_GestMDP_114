705ef08870acac7d3870554f24ccb9be42fbc673
Amir Zeqiri <amir.zeqiri@eduvaud.ch>
2024-09-02 12:33:18 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿/******************************************************************************
+** PROJET  Gestionnaire de mot de passe - 114                                **
+**                                                                           **
+** Lieu      : ETML - Vennes                                                 **
+** Auteur    : Amir Zeqiri                                                   **
+** Date      : 26.08.2024 - 28.10.2024                                       **
+******************************************************************************/
+/******************************************************************************
+** DESCRIPTION Faire un gestionnaire de mot de passe                         **
+******************************************************************************/
+
+
+using System;
+
+namespace P_GestMDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // Efface l'écran
+            Console.Clear();
+
+            Console.WriteLine("*******************************\nSélectionnez une action\n1. Consulter un mot de passe\n2. Ajouter un mot de passe\n3. Supprimer un mot de passe\n4. Quitter le programme\n*******************************\n\nFaites votre choix : ");
+        }
+    }
+}
