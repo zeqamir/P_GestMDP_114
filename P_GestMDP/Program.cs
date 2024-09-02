89905c5b97aaa5b666af49b14c5d9771566d0a39
Amir Zeqiri <amir.zeqiri@eduvaud.ch>
2024-09-02 14:42:17 +0200

    Création du menu brut

--- P_GestMDP/Program.cs
+++ P_GestMDP/Program.cs
@@ -1,13 +1,7 @@
-﻿/******************************************************************************
-** PROJET  Gestionnaire de mot de passe - 114                                **
-**                                                                           **
-** Lieu      : ETML - Vennes                                                 **
-** Auteur    : Amir Zeqiri                                                   **
-** Date      : 26.08.2024 - 28.10.2024                                       **
-******************************************************************************/
-/******************************************************************************
-** DESCRIPTION Faire un gestionnaire de mot de passe                         **
-******************************************************************************/
+﻿///ETML - Vennes
+///Auteur : Amir Zeqiri
+///Date : 26.08.2024 - 28.10.2024
+///Description : Gestionnaire de mot de passe - 114
 
 
 using System;
@@ -20,7 +14,70 @@ namespace P_GestMDP
         {   // Efface l'écran
             Console.Clear();
 
-            Console.WriteLine("*******************************\nSélectionnez une action\n1. Consulter un mot de passe\n2. Ajouter un mot de passe\n3. Supprimer un mot de passe\n4. Quitter le programme\n*******************************\n\nFaites votre choix : ");
+            char Reponse; // Variable du recommencement du programme
+
+            do
+            {
+                Console.Clear();
+                Console.Write("*******************************" +
+                    "\nSélectionnez une action" +
+                    "\n1. Consulter les mots de passe" +
+                    "\n2. Ajouter un mot de passe" +
+                    "\n3. Supprimer un mot de passe" +
+                    "\n4. Quitter le programme" +
+                    "\n*******************************" +
+                    "\n\nFaites votre choix : ");
+
+                string Choix = Console.ReadLine();
+                if (Choix == "1")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Vos mots de passe : " +
+                        "\n\n 1." +
+                        "\n URL : www.login.com" +
+                        "\n Identifiant : user" +
+                        "\n Mot de passe (non chiffré) : password" +
+                        "\n\n 2." +
+                        "\n URL : www.google.com" +
+                        "\n Identifiant : admin" +
+                        "\n Mot de passe (non chiffré) : pass" +
+                        "\n\n\n Quel mot de passe voulez-vous modifier ? : ");
+                    Console.Read();
+                }
+
+                if (Choix == "2")
+                {
+                    Console.Clear();
+                    Console.Write("Veuillez entrez l'URL du site : ");
+                    Console.Read();
+                }
+
+                if (Choix == "3")
+                {
+                    Console.WriteLine("Vos mots de passe : " +
+                        "\n\n 1." +
+                        "\n URL : www.login.com" +
+                        "\n Identifiant : user" +
+                        "\n Mot de passe (non chiffré) : password" +
+                        "\n\n 2." +
+                        "\n URL : www.google.com" +
+                        "\n Identifiant : admin" +
+                        "\n Mot de passe (non chiffré) : pass" +
+                        "\n\n\n Quel mot de passe voulez-vous supprimer ? : ");
+                    Console.Read();
+                }
+
+                if (Choix == "4")
+                {
+                    Environment.Exit(0);
+                }
+
+                // Affichage et saisie pour recommencer
+                Console.Write("\nVoulez-vous relancer le programme (o / n): ");
+                Reponse = Convert.ToChar(Console.ReadLine());
+            }
+            // Boucle de recommencement
+            while (Reponse == 'o' || Reponse == '0');
         }
     }
 }
